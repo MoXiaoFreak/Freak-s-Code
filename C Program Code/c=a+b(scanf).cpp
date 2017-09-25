7da99719fed1c4286e3a6514f8c63e26da5f62f6
MoXiaoFreak <2714863331@qq.com>
2017-09-26 00:56:51 +0800

    Adjustment 3

---
+++ C Program Code/c=a+b(scanf).cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("please input the first munber:\n");
+	scanf("%d",&a);
+    printf("please input the second munber:\n");
+	scanf("%d",&b);
+	c=a+b;
+	printf("c=%d\n",c);
+	return 0;
+}
