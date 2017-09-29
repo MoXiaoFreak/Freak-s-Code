76f64b7a78b45c1e1652d4c352c274225522c905
MoXiaoFreak <2714863331@qq.com>
2017-09-29 11:28:49 +0800

    A
    q

--- C Program Code/SimpleMax(With Child Function).cpp
+++ C Program Code/SimpleMax(With Child Function).cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-float max(float x, float y);
-float max(float x, float y)
+float Max(float x, float y);
+float Max(float x, float y)
 {
 	/*       if (x > y)
-	return x;
-	else
-	return y;
+	                  return x;
+	          else
+	                  return y;
 	*/
 	return x > y ? x : y;
 }
@@ -16,7 +16,7 @@ int main()
 	scanf("%f", &x);
 	printf("Please input y:\n");
 	scanf("%f", &y);
-	c = max(x, y);
+	c = Max(x, y);
 	printf("Max of (%f,%f) is %f\n", x, y, c);
 	return 0;
 }
