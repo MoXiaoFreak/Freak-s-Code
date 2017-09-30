c396345da7d18ebdac3270b19e6e22127bd7fd9f
MoXiaoFreak <2714863331@qq.com>
2017-09-30 10:27:15 +0800

    ADJUSTMENT

---
+++ C Program Code/BOOK 2.17(c).cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("%d", 1);
+	printf("%d", 2);
+	printf("%d", 3);
+	printf("%d", 4);
+	return 0;
+}
--- C Program Code/Desperate a number.cpp
+++ C Program Code/Desperate a number.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
-int main()
+void desperate(int a);
+void desperate(int a)
 {
-	unsigned int a;
-	int b, i;
-	printf("Please input a five-digit integer:\n");
-	scanf("%d", &a);
-	for (i = 10000;i >=1;i /=10)
+	int i, b;
+	for (i = 10000;i >= 1;i /= 10)
 	{
 		b = a / i;
 		a = a%i;
-		printf("%d      ", b);
+		printf("%d   ", b);
 	}
+}
+int main()
+{
+	int a;
+	printf("Please input a five-digit integer:\n");
+	scanf("%d", &a);
+	desperate(a);
 	printf("\n");
 	return 0;
 }
