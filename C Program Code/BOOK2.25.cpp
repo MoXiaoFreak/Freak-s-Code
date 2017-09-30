972fd6e5f413a095a19bc73af5930a00c16b2f93
MoXiaoFreak <2714863331@qq.com>
2017-09-30 10:37:27 +0800

    dyf

---
+++ C Program Code/BOOK2.25.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	printf("DDDDDDDD\n");
+	printf("D      D\n");
+	printf("D      D\n");
+	printf(" D    D \n");
+  printf("  DDDD  \n");
+	printf("        \n");
+	printf("       Y\n");
+	printf("      Y \n");
+	printf("YYYYYY  \n");
+	printf("      Y \n");
+	printf("       Y\n");
+  printf("        \n");
+  printf("FFFFFFFF\n");
+	printf("    F  F\n");
+	printf("    F  F\n");
+	printf("    F  F\n");
+	printf("    F  F\n");
+	return 0;
+}
