be5e439a9bbc85043398b93424e10e466b154f2e
MoXiaoFreak <2714863331@qq.com>
2017-10-13 11:33:09 +0800

    A

--- C Program Code/Guessing a given random number(Full ver.).cpp
+++ C Program Code/Guessing a given random number(Full ver.).cpp
@@ -5,12 +5,11 @@
 int main()
 {
 	int random, i = 1;
-	while(i = 1)
+	while(i == 1)
 	{
 		srand(time(0));
 		random = rand() % 101;
 		int input, count = 1;
-		printf("*************Freak's Guessing Game*************\n");
 		printf("There is a real random number.Please input a 0-100 number(an integer) to guess it:\n");
 		scanf("%d", &input);
 		for (;input != random;)
