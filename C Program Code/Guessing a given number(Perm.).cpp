4a030901bb46b192c4681d35d9fe625c2820b705
MoXiaoFreak <2714863331@qq.com>
2017-10-13 09:38:03 +0800

    Update

--- C Program Code/Guessing a given number(Perm.).cpp
+++ C Program Code/Guessing a given number(Perm.).cpp
@@ -1,17 +1,27 @@
 #include <stdio.h>
 int main()
 {
-	int givennumber = 5,input;
+	int givennumber = 5, input;
 	printf("There is a random(HA?) number.Please input a number(an integer) to guess it:\n");
-	scanf("%d",&input);
+	scanf("%d", &input);
+    for (;input != givennumber;)
+	{
+		if (input > givennumber)
+			printf("Too big,try another:\n");
+		else if (input < givennumber)
+			printf("Too small,too simple,try another:\n");
+		scanf("%d", &input);
+	}
+	/*
 	while (input != givennumber)
 	{
 		if (input > givennumber)
 			printf("Too big,try another:\n");
 		else if (input < givennumber)
-			printf("Too small,try another:\n");
+			printf("Too small,too simple,try another:\n");
 		scanf("%d", &input);
 	}
+	*/
 	if (input == givennumber)
 		printf("Congratulations!\n");
 	return 0;
