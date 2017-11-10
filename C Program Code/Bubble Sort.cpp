30c58a8134f5f09fa87b047342f0a812b4a66376
MoXiaoFreak <2714863331@qq.com>
2017-11-10 11:21:55 +0900

    Update

--- C Program Code/Bubble Sort.cpp
+++ C Program Code/Bubble Sort.cpp
@@ -1,10 +1,11 @@
-#include <stdio.h>
-void Sort(int a[],int k);
-void Sort(int a[],int k)
+#i#include <stdio.h>
+void Sort(int a[], int k);
+int findFirst(int a[], int n, int value);
+void Sort(int a[], int k)
 {
 	int i, j, t;
-	for (j = 1;j <= k-2;j++)
-		for (i = 0;i <= k-2;i++)
+	for (j = 1;j <= k - 2;j++)
+		for (i = 0;i <= k - 2;i++)
 		{
 			if (a[i] >= a[i + 1])
 			{
@@ -16,14 +17,28 @@ void Sort(int a[],int k)
 	for (i = 0;i <= 9;i++)
 		printf("%d   ", a[i]);
 }
+int findFirst(int a[], int n, int value)
+{
+	int z, i;
+	for (i = 0;i <= n - 1;i++)
+		if (a[i] == value)
+			break;
+	z = i;
+	return z;
+}
 int main()
 {
-	int a[10], i;
+	int a[10], i,k,j;
+	printf("input the number that you want to know its position after sorting:");
+	scanf("%d", &j);
+	printf("input 10 numbers;");
 	for (i = 0;i <= 9;i++)
 	{
 		scanf("%d", &a[i]);
 	}
-	Sort(a,10);
+	Sort(a, 10);
+	k = findFirst(a, 10, j);
+	printf("%d",k);
 	printf("\n");
 	return 0;
 }
